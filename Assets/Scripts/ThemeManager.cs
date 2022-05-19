6b9dcbdb167b62c730dd54d62e4584686cf37872
SCG-Priyanka <98299805+SCG-Priyanka@users.noreply.github.com>
2022-05-19 11:34:48 +0530

    Modified new theme unlock mechanism

--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -16,22 +16,21 @@ public class ThemeManager : MonoBehaviour
 
         int currentLevel = PlayerPrefs.GetInt("current_level", 0);
         int currentTheme = PlayerPrefs.GetInt("current_theme", 0);
-
-        
         themeObjects[currentTheme].SetActive(true);
-            
-         
-        if(setCurrent)
-        {
-            if ((currentLevel % 3 == 0) && currentLevel!=0)
-            {
-                currentTheme++;
-            }
 
-            currentTheme = currentTheme % themeObjects.Count;
+
+        if ((currentLevel % 1 == 0) && currentLevel!=0)
+        {
+            themeObjects[currentTheme].SetActive(false);
+
+            currentTheme++;
+            if(currentTheme == themeObjects.Count)
+            {
+                currentTheme = 0;
+            }
+            themeObjects[currentTheme].SetActive(true);
             PlayerPrefs.SetInt("current_theme", currentTheme);
         }
-
     }
 
 
