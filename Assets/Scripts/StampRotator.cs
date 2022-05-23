5ef0d00d6a34bfa3c229dc8ad9d4a99c4feba115
gamedevnicky1998 <gamedevnicky1998@gmail.com>
2022-05-23 11:56:53 +0530

    Minor

--- Assets/Scripts/StampRotator.cs
+++ Assets/Scripts/StampRotator.cs
@@ -20,13 +20,17 @@ public class StampRotator : MonoBehaviour
         float t = 0f;
         Quaternion initRot = transform.rotation;
         Quaternion targetRot = initRot * Quaternion.Euler(0, 0, 180);
-
+        float initZ = transform.position.z;
+        float targetZ = 6.7f;
 
         while (t<(timeOFlight))
         {
             var x = Mathf.Clamp(t / timeOFlight, 0f, 1f);
             transform.rotation = Quaternion.Lerp(initRot, targetRot, x);
             transform.position += new Vector3(0, velocity * Time.deltaTime, 0);
+            var p = transform.position;
+            p.z = Mathf.Lerp(initZ,targetZ,x);
+            transform.position = Vector3.Lerp(transform.position, p, x);
             t += Time.deltaTime;
             velocity -= Time.deltaTime * gravity;
 
@@ -34,6 +38,9 @@ public class StampRotator : MonoBehaviour
         }
 
         transform.position = initialPos+new Vector3(0,0.4f,0);
+        var p2 = transform.position;
+        p2.z = 6.7f;
+        transform.position = p2;
         transform.rotation = targetRot;
 
 
